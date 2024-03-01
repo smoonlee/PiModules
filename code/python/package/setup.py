91c6a2e904862aa8c42409af98de1dc7fb6a9df2
Simon Lee <simon@leefamily.uk.net>
2024-03-01 19:07:06 +0000

    Update setup.py

--- code/python/package/setup.py
+++ code/python/package/setup.py
@@ -1,4 +1,3 @@
-
 """pimodules: PiModules(R) Product support Python package
 
 A package which contains code to support PiModules products of various kinds.
@@ -7,27 +6,32 @@ A package which contains code to support PiModules products of various kinds.
 classifiers = """\
 Development Status :: 5 - Testing/Beta
 Intended Audience :: PiModules Product Developers
-License :: GNU General Public License Version 3
-Programming Language :: Python >= 2.7
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Topic :: PiModules(R)
 Topic :: PiModules(R) Products Python Package
-Operating System :: Linux (Raspbian)
+Operating System :: POSIX :: Linux
 """
 
-
-from distutils.core import setup
+from setuptools import setup
 
 doclines = __doc__.split("\n")
 
-setup(name='pimodules',
-      version='0.1dev',
-      description=doclines[0],
-      long_description = "\n".join(doclines[2:]),
-      license='GPL3',
-      author='Mike Ray',
-      author_email='mike.ray@btinternet.com',
-      url='http://pimodules.com',
-      platforms=['POSIX'],
-      classifiers = filter(None, classifiers.split("\n")),
-      packages=['pimodules'],
-      )
+setup(
+    name='pimodules',
+    version='0.1dev',
+    description=doclines[0],
+    long_description="\n".join(doclines[2:]),
+    license='GPL3',
+    author='Mike Ray',
+    author_email='mike.ray@btinternet.com',
+    url='http://pimodules.com',
+    platforms=['POSIX'],
+    classifiers=classifiers.split('\n'),
+    packages=['pimodules'],
+    python_requires='>=3.6',
+)
